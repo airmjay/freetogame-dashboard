9bf02af66dbe4ff783d1e365657ee286673145d2
airmjay <abdulmojeedaliyu@gmail.com>
2026-09-07 12:30:49 +0100

    Make little update on game card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -7,7 +7,7 @@ export const GameCard = ({ item }: props) => {
   return (
     <div className="dark:bg-gray-600 dark:text-white dark:border dark:border-slate-100 card bg-base-100 w-auto shadow-sm hover:scale-101 cursor-pointer">
       <figure>
-        <img src={item.thumbnail} alt={item.title} />
+        <img className="w-full" src={item.thumbnail} alt={item.title} />
       </figure>
       <div className="card-body">
         <div className="text-sm dark:text-white card-title flex flex-wrap">
